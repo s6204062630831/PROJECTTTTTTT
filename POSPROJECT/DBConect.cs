109183b7e3c1381d89a998c45bae0429ab94dd8e
PRAKRIT TAEMJUN <s6204062630831@kmutnb.ac.th>
2021-05-18 17:45:03 +0700

    Add project files.

---
+++ POSPROJECT/DBConect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace POSPROJECT
+{
+    class DBConect
+    {
+        private SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\punzh\source\repos\POSPROJECT\POSPROJECT\Database1.mdf;Integrated Security=True");
+        
+        public SqlConnection Getcon()
+        {
+            return connection;
+        }
+        public void OpenCon()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+               connection.Open();
+            }
+        }
+        public void CloseCon()
+        {
+            if(connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+
+}
